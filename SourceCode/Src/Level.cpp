7cf6b8cf052401259452155ecee18e3d75da68c0
Zanbuffet <Zanbuffet@126.com>
2018-07-12 16:04:16 +0800

    test commit

--- SourceCode/Src/Level.cpp
+++ SourceCode/Src/Level.cpp
@@ -31,4 +31,5 @@ void Level::loadMap(std::string songInfoFilePath)
 	//把json里面的数据一一对应到这个类的各个成员
 	this->songName = root["songName"].asString();
 	this->songTempo = root["songTempo"].asFloat();
+	//尝试新东西
 }
